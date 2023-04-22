fbc5a5b1037d5d09856dfc71abb0a16461d08663
GrKoR <dr.g.kornev@gmail.com>
2023-04-22 20:39:03 +0400

    upd: pix_utils swap endian for int16/32

--- pix_utils.h
+++ pix_utils.h
@@ -14,11 +14,23 @@ inline void swap_endian(uint16_t &val)
     val = (val << 8) | (val >> 8);
 }
 
+// swaps int16 endian
+inline void swap_endian(int16_t &val)
+{
+    swap_endian(*(uint16_t*)&val);
+}
+
 // swaps uint32 endian
 inline void swap_endian(uint32_t &val)
 {
     val = (val << 24) | ((val << 8) & 0x00ff0000) |
           ((val >> 8) & 0x0000ff00) | (val >> 24);
+}
+
+// swaps int32 endian
+inline void swap_endian(int32_t &val)
+{
+    swap_endian(*(uint32_t*)&val);
 }
 
 #endif // PIX_UTILS_H
