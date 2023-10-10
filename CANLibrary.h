c340169f4666ef01a40db1668e52c068906008bd
GrKoR <dr.g.kornev@gmail.com>
2023-10-10 09:23:59 +0300

    ref: temporary reverse change of GetValue GetValue changed to GetTypedValue for the purpose of derived projects fixing

--- CANLibrary.h
+++ CANLibrary.h
@@ -7,10 +7,4 @@
 #include "CANManager.h"
 #include "CAN_common_block.h"
 
-/*
-#warning In CANLibrary v0.1.12 names of methods was changed
-#warning CANObject.GetValue() is CANObject.GetValuePtr() now, CANObject.GetTypedValue() is CANObject.GetValue() now
-#warning Be careful, it may cause the runtime error
-*/
-
 #endif // CANLIBRARY_H
